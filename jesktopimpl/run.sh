a1f0d119e682540b7eced4004017ee77bc2d40e0
paul-h <paul-h@f5eb1be0-8cbc-421e-ae54-248d26caedf7>
2004-02-16 23:09:01 +0000

    build stuff

---
+++ jesktopimpl/run.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+java -classpath lib/picocontainer-1.0-beta-5-SNAPSHOT.jar;lib/nanocontainer-1.0-beta-1-SNAPSHOT.jar;lib/spice-cli-1.0.jar;lib/js-1.5R4.1.jar;lib/jesktop.jar;lib/jesktop-api.jar;lib/jesktop-frimble.jar;lib/avalon-framework-20022103.jar org.nanocontainer.main.NanoContainerMain -c composition.js
