1ad02dacc3dc13bf9f9e12f9475bddff806a57f3
paul-h <paul-h@f5eb1be0-8cbc-421e-ae54-248d26caedf7>
2005-12-18 10:11:50 +0000

    jar moves

--- jesktopimpl/run.sh
+++ jesktopimpl/run.sh
@@ -3,7 +3,7 @@
 # NanoContainer Booter script v 1.0-RC-3
 # www.nanocontainer.org
 
-EXEC="$JAVA_HOME/bin/java -Djava.security.manager -Djava.security.policy=file:lib/booter.policy -jar lib/nanocontainer-booter-1.0-RC-3.jar $@"
+EXEC="$JAVA_HOME/bin/java -Djava.security.manager -Djava.security.policy=file:lib/booter.policy -jar lib/nanocontainer-booter-1.0-RC-3.jar -c composition.xml"
 echo $EXEC
 $EXEC
 
